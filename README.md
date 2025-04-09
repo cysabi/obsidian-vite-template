4a6d5f7b64bfe7feec5c7e6328578877b2df185b
cysabi <47790183+cysabi@users.noreply.github.com>
2025-04-08 20:18:50 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# `obsidian-vite-template`
+> a modern plugin template for obsidian
+
+- Minimal, everything is handled inside of `vite.config.mjs`
+- Write files to an `outDir` instead of developing in your obsidian vault
+- `manifest.json` is automatically generated from your `package.json`
+
+## usage
+For help with writing plugins, see https://github.com/obsidianmd/obsidian-sample-plugin/blob/master/README.md
+
+- Use this template
+- Edit `outDir` in `vite.config.mts`
+- Replace all of the manifest values in `package.json`
+- `npm i`
+- `npx vite build --watch` or `npx vite build` for production
+
+---
+
+_empathy included • [**@cysabi**](https://github.com/cysabi) • [cysabi.github.io](https://cysabi.github.io)_
---
+++ vite.config.mjs
@@ -0,0 +1,79 @@
+import { defineConfig } from "vite";
+import builtins from "builtin-modules";
+import { resolve } from "node:path";
+import { readFileSync } from "node:fs";
+
+const outDir = ""; // TODO
+
+export default defineConfig({
+  plugins: [writeObsidianAssets()],
+  build: {
+    outDir,
+    target: "es2018",
+    cssCodeSplit: true,
+    lib: {
+      entry: ["src/main.ts", "src/styles.css"],
+      formats: ["cjs"],
+    },
+    sourcemap: "inline",
+    rollupOptions: {
+      external: [
+        "obsidian",
+        "electron",
+        "@codemirror/autocomplete",
+        "@codemirror/closebrackets",
+        "@codemirror/collab",
+        "@codemirror/commands",
+        "@codemirror/comment",
+        "@codemirror/fold",
+        "@codemirror/gutter",
+        "@codemirror/highlight",
+        "@codemirror/history",
+        "@codemirror/language",
+        "@codemirror/lint",
+        "@codemirror/matchbrackets",
+        "@codemirror/panel",
+        "@codemirror/rangeset",
+        "@codemirror/rectangular-selection",
+        "@codemirror/search",
+        "@codemirror/state",
+        "@codemirror/stream-parser",
+        "@codemirror/text",
+        "@codemirror/tooltip",
+        "@codemirror/view",
+        "@lezer/common",
+        "@lezer/highlight",
+        "@lezer/lr",
+        ...builtins,
+      ],
+    },
+  },
+});
+
+function writeObsidianAssets() {
+  return {
+    name: "writeObsidianAssets",
+    apply: "build",
+    async generateBundle() {
+      const pkg = JSON.parse(
+        readFileSync(resolve(process.cwd(), "package.json"), "utf-8")
+      );
+
+      const manifest = {
+        id: pkg.name,
+        version: pkg.version,
+        description: pkg.description,
+        author: pkg.author.name,
+        authorUrl: pkg.author.url,
+        fundingUrl: pkg.funding,
+        ...pkg.obsidian,
+      };
+      this.emitFile({ type: "asset", fileName: ".hotReload" });
+      this.emitFile({
+        type: "asset",
+        fileName: "manifest.json",
+        source: JSON.stringify(manifest, null, "\t"),
+      });
+    },
+  };
+}
